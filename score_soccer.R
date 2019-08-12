fa25c141ab0b5a4fb845c41c2f02cc42ebae4b45
crashcarson15 <53834975+crashcarson15@users.noreply.github.com>
2019-08-12 15:24:43 -0400

    Add files via upload

--- score_soccer.R
+++ score_soccer.R
@@ -5,23 +5,33 @@ require(dplyr)
 require(googlesheets)
 
 ### define our macro variables here
-league <- "FBW"
+league <- "BIP"
+match_dates <- c("Aug 9, 2019", "Aug 10, 2019", "Aug 11, 2019")
+matchday <- "MD1"
 
-match_dates <- c("May 18, 2019")
-comp <- "Bund"
-
+if (league == "FBW") {
+  comp <- "Bund"
+  sheet <- "2019/20 Fußball-Weltliga"
+  player_count <- 72
+  lineup_range <- "E2:I74"
+} else if (league == "BIP") {
+  comp <- "Prem"
+  sheet <- "2019/20 British Isles Premiership"
+  player_count <- 72
+  lineup_range <- "E2:I74"
+}
 
 # first, import our lineups for the week
-lineups <- gs_read(ss = gs_title("2019/20 Fußball-Weltliga"), ws = "[DATA] Lineups", range = "E2:I74")
+lineups <- gs_read(ss = gs_title(sheet), ws = "[DATA] Lineups", range = lineup_range)
 
 # then, tack on the 2019 suffix to the URL to make sure we default to the right screen (this matters later in the season)
-lineups$espn <- paste0(lineups$espn, "?season=2018")
+lineups$espn <- paste0(lineups$espn, "?season=2019")
 
 # now, initialize some blank vectors
-appear <- as.character(rep("", 72))
+appear <- as.character(rep("", player_count))
 goals <- assists <-
   cs <- gc <- win <- draw <- saves <-
-    rep(NA, 72)
+    rep(NA, player_count)
 
 vectors <- data.frame(appear, goals, assists, cs, gc, win, draw, saves, stringsAsFactors = FALSE)
 
@@ -29,7 +39,7 @@ toscore <- cbind(lineups, vectors)
 
 
 
-for (i in 1:72) {
+for (i in 1:player_count) {
   # first, score the goalkeepers
   if (i %% 9 == 1) {
     # bring in the player's match log
@@ -43,7 +53,7 @@ for (i in 1:72) {
     competitions <- c(read_in[4], read_in[15])
     
     # did the player play this matchday?
-    if (game_dates[1] %in% match_dates && competitions[1] == "Bund") {
+    if (game_dates[1] %in% match_dates && competitions[1] == comp) {
       toscore[i,]$appear <- read_in[6]
       toscore[i,]$saves <- as.numeric(read_in[7])
       
@@ -51,15 +61,20 @@ for (i in 1:72) {
         
         if (substr(result, 1, 1) == "W") {
           toscore[i,]$win <- 1
-          toscore[i,]$gc <- as.numeric(substr(result, 5, 5))
+          toscore[i,]$gc <- min(as.numeric(substr(result, 3, 3)), as.numeric(substr(result, 5, 5)))
         } else if (substr(result, 1, 1) == "D") {
-          toscore[i,]$win <- 1
+          toscore[i,]$draw <- 1
           toscore[i,]$gc <- as.numeric(substr(result, 5, 5))
         } else {
-          toscore[i,]$gc <- as.numeric(substr(result, 3, 3))
+          toscore[i,]$gc <- max(as.numeric(substr(result, 3, 3)), as.numeric(substr(result, 5, 5)))
         }
       
-    } else if (game_dates[2] %in% match_dates && competitions[2] == "Bund") { # double check the previous entry in case we're late scoring
+      # if the player didn't play, blank out all result values
+      if (toscore[i,]$appear == "Unused Substitute") {
+        toscore[i,]$gc <- toscore[i,]$cs <- NA
+      }
+      
+    } else if (game_dates[2] %in% match_dates && competitions[2] == comp) { # double check the previous entry in case we're late scoring
       toscore[i,]$appear <- read_in[17]
       toscore[i,]$saves <- as.numeric(read_in[18])
       
@@ -69,10 +84,15 @@ for (i in 1:72) {
         toscore[i,]$win <- 1
         toscore[i,]$gc <- min(as.numeric(substr(result, 3, 3)), as.numeric(substr(result, 5, 5)))
       } else if (substr(result, 1, 1) == "D") {
-        toscore[i,]$win <- 1
+        toscore[i,]$draw <- 1
         toscore[i,]$gc <- as.numeric(substr(result, 5, 5))
       } else {
         toscore[i,]$gc <- max(as.numeric(substr(result, 3, 3)), as.numeric(substr(result, 5, 5)))
+      }
+      
+      # if the player didn't play, blank out all result values
+      if (toscore[i,]$appear == "Unused Substitute") {
+        toscore[i,]$gc <- toscore[i,]$cs <- NA
       }
       
     } else {
@@ -93,7 +113,7 @@ for (i in 1:72) {
   competitions <- c(read_in[4], read_in[18])
   
     # did the player play this matchday?
-    if (game_dates[1] %in% match_dates && competitions[1] == "Bund") {
+    if (game_dates[1] %in% match_dates && competitions[1] == comp) {
       toscore[i,]$appear <- read_in[6]
       toscore[i,]$goals <- as.numeric(read_in[7])
       toscore[i,]$assists <- as.numeric(read_in[8])
@@ -114,7 +134,12 @@ for (i in 1:72) {
           toscore[i,]$cs <- 0
         }
       }
-    } else if (game_dates[2] %in% match_dates && competitions[2] == "Bund") { # double check the previous entry in case we're late scoring
+      # if the player didn't play, blank out all result values
+      if (toscore[i,]$appear == "Unused Substitute") {
+        toscore[i,]$gc <- toscore[i,]$cs <- NA
+      }
+      
+    } else if (game_dates[2] %in% match_dates && competitions[2] == comp) { # double check the previous entry in case we're late scoring
       toscore[i,]$appear <- read_in[20]
       toscore[i,]$goals <- as.numeric(read_in[21])
       toscore[i,]$assists <- as.numeric(read_in[22])
@@ -135,6 +160,11 @@ for (i in 1:72) {
           toscore[i,]$cs <- 0
         }
       }
+      
+      # if the player didn't play, blank out all result values
+      if (toscore[i,]$appear == "Unused Substitute") {
+        toscore[i,]$gc <- toscore[i,]$cs <- NA
+      }
     } else {
       toscore[i,]$appear <- "Not in Squad"
     }
@@ -147,4 +177,4 @@ toexport <- toscore %>% select(espn, appear, goals, assists, cs, gc, win, draw, 
 toexport[is.na(toexport)] <- ""
 
 # Then, push back to where we're going
-gs_edit_cells(ss = gs_title("2019/20 Fußball-Weltliga"), ws = "MD1", input = toexport, anchor = "AQ11")
+gs_edit_cells(ss = gs_title(sheet), ws = matchday, input = toexport, anchor = "AQ10")
